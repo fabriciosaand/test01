83ab665831e4c96349d8574a73c80cd5e77059f6
Fabricio Andrade <fabriciosaand@gmail.com>
2022-10-02 09:22:09 -0300

    package war

--- src/main/java/com/fabriciosaand/testedocker/TestedockerApplication.java
+++ src/main/java/com/fabriciosaand/testedocker/TestedockerApplication.java
@@ -2,9 +2,10 @@ package com.fabriciosaand.testedocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TestedockerApplication {
+public class TestedockerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestedockerApplication.class, args);
