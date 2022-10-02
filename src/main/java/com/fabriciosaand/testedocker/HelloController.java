44bcbaf553decc974881256ed7e474d92df58de3
Fabricio Andrade <fabriciosaand@gmail.com>
2022-10-02 09:49:30 -0300

    added log

--- src/main/java/com/fabriciosaand/testedocker/HelloController.java
+++ src/main/java/com/fabriciosaand/testedocker/HelloController.java
@@ -1,13 +1,16 @@
 package com.fabriciosaand.testedocker;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello(){
+        log.info("Greetings from Spring Boot!");
         return "Greetings from Spring Boot!";
     }
 }
